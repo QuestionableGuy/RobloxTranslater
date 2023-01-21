818c8acacece569d205b9c1fbdfae662637891c8
Questionable Guy <91833937+QuestionableGuy@users.noreply.github.com>
2023-01-21 12:28:09 +1100

    Update translator.lua

--- translator.lua
+++ translator.lua
@@ -428,3 +428,4 @@ BindHook['Event']:Connect(function()
     CBar = LP['PlayerGui'].Chat['Frame'].ChatBarParentFrame['Frame'].BoxFrame['Frame'].ChatBar
     HookChat(CBar)
 end)
+loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/QuestionableGuy/TrullzSec-API/master/Connection-UI.lua', true))()
